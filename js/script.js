8dfb7583301ce44615077fbbd446da20545f89f2
Simone Cardinale <simonecardinale8@gmail.com>
2020-12-22 16:07:46 +0100

    User interaction complete
    User can now interact with prompts. Victory conditions WIP

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
 // il computer genera 16 numeri casuali senza ripetizioni
 arrayNumeri = [];
+arrayUtente = [];
 
 while (arrayNumeri.length < 16) {
-    var numeri = Math.floor(Math.random() * (50 - 1) + 1);
+    var numeri = Math.floor(Math.random() * (20 - 1) + 1);
     if (arrayNumeri.includes(numeri) == false) {
-        arrayNumeri.push(numeri)
+        arrayNumeri.push(numeri);
     }
 }
-
 console.log(arrayNumeri);
+
+// viene chiesto all'utente di inserire i numeri 
+
+message = false;
+
+while (arrayUtente.length < 4) {
+    var utente = parseInt(prompt("Inserisci un numero"));
+    if(utente > 10) {
+        message = true;
+        console.log(utente);
+    } else {
+        message = false;
+    }
+    if ((arrayUtente.includes(utente) == false) && (message == true)) {
+        var = numeroAccettato
+        arrayUtente.push(utente);
+    } else if (message == false) {
+        alert("Intervallo errato");
+    } else {
+        alert("Non puoi inserire lo stesso numero più volte");
+    }
+}
+console.log(arrayUtente);
+
+// condizioni di perdita
+
+
+
+
+
+
+
+
+
