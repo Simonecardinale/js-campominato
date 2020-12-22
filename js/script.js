98c9aabc137f8a450696608be641d44ff31cdde6
Simone Cardinale <simonecardinale8@gmail.com>
2020-12-22 18:14:26 +0100

    work complete + bonus
    Comments WIP

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@
 arrayNumeri = [];
 arrayUtente = [];
 
-while (arrayNumeri.length < 16) {
-    var numeri = Math.floor(Math.random() * (100 - 1) + 1);
-    if (arrayNumeri.includes(numeri) == false) {
-        arrayNumeri.push(numeri);
-    }
-}
-console.log(arrayNumeri);
 
+
+// chiedo all'utente la difficoltà 
+
+var difficoltà = parseInt(prompt("Segli la difficoltà inserendo 1, 2 o 3"));
+
+while (difficoltà != 1 && difficoltà != 2 && difficoltà != 3) {
+    alert("Scegli la difficoltà giusta");
+    var difficoltà = parseInt(prompt("Segli la difficoltà inserendo 1, 2 o 3"));
+}
 
 
 
@@ -23,37 +25,165 @@ var message = false;
 var semaforo = true;
 
 
-while (arrayUtente.length < 84 && semaforo == true) {
-    var utente = parseInt(prompt("Inserisci un numero"));
+switch(difficoltà) {
+    case 1:
+        while (arrayNumeri.length < 16) {
+            var numeri = Math.floor(Math.random() * (100 - 1) + 1);
+            if (arrayNumeri.includes(numeri) == false) {
+                arrayNumeri.push(numeri);
+            }
+        }
+        console.log(arrayNumeri);
 
-    for (i = 0; i < arrayNumeri.length; i++) {
-        if (arrayNumeri[i] == utente) {
-            semaforo = false;
-            alert("hai perso");
-        } 
-    }
-    if (semaforo == true) {
-        if(utente >= 1 && utente <= 100) {
-            message = true;
-            // console.log(utente);
-        } else {
-            message = false;
+        while (arrayUtente.length < 84 && semaforo == true) {
+            var utente = parseInt(prompt("Inserisci un numero"));
+        
+            for (i = 0; i < arrayNumeri.length; i++) {
+                if (arrayNumeri[i] == utente) {
+                    semaforo = false;
+                    alert("hai perso");
+                } 
+            }
+            if (semaforo == true) {
+                if(utente >= 1 && utente <= 100) {
+                    message = true;
+                    // console.log(utente);
+                } else {
+                    message = false;
+                }
+                if ((arrayUtente.includes(utente) == false) && (message == true)) {
+                    arrayUtente.push(utente);
+                } else if (message == false) {
+                    alert("Intervallo errato");
+                } else {
+                    alert("Non puoi inserire lo stesso numero più volte");
+                }
+            }
         }
-        if ((arrayUtente.includes(utente) == false) && (message == true)) {
-            arrayUtente.push(utente);
-        } else if (message == false) {
-            alert("Intervallo errato");
-        } else {
-            alert("Non puoi inserire lo stesso numero più volte");
+        
+        console.log(arrayUtente);
+        
+        // definisco il punteggio finale 
+        
+        for (i = 0; i < arrayUtente.length; i++) {
+            var vincitore = (i + 1);
         }
-    }
+        
+        document.getElementById('punteggio').innerHTML += "Il tuo punteggiò è di " + vincitore;
+        
+        
+        if (vincitore == 84) {
+            document.getElementById('campione').innerHTML = "Complimenti hai vinto!"
+        }
+
+    break;
+
+    case 2 :
+        while (arrayNumeri.length < 16) {
+            var numeri = Math.floor(Math.random() * (80 - 1) + 1);
+            if (arrayNumeri.includes(numeri) == false) {
+                arrayNumeri.push(numeri);
+            }
+        }
+        console.log(arrayNumeri);
+
+        while (arrayUtente.length < 64 && semaforo == true) {
+            var utente = parseInt(prompt("Inserisci un numero"));
+        
+            for (i = 0; i < arrayNumeri.length; i++) {
+                if (arrayNumeri[i] == utente) {
+                    semaforo = false;
+                    alert("hai perso");
+                } 
+            }
+            if (semaforo == true) {
+                if(utente >= 1 && utente <= 80) {
+                    message = true;
+                    // console.log(utente);
+                } else {
+                    message = false;
+                }
+                if ((arrayUtente.includes(utente) == false) && (message == true)) {
+                    arrayUtente.push(utente);
+                } else if (message == false) {
+                    alert("Intervallo errato");
+                } else {
+                    alert("Non puoi inserire lo stesso numero più volte");
+                }
+            }
+        }
+        
+        console.log(arrayUtente);
+        
+        // definisco il punteggio finale 
+        
+        for (i = 0; i < arrayUtente.length; i++) {
+            var vincitore = (i + 1);
+        }
+        
+        document.getElementById('punteggio').innerHTML += "Il tuo punteggiò è di " + vincitore;
+        
+        
+        if (vincitore == 64) {
+            document.getElementById('campione').innerHTML = "Complimenti hai vinto!"
+        }
+
+        break;
+        
+        case 3:
+
+        while (arrayNumeri.length < 16) {
+            var numeri = Math.floor(Math.random() * (50 - 1) + 1);
+            if (arrayNumeri.includes(numeri) == false) {
+                arrayNumeri.push(numeri);
+            }
+        }
+        console.log(arrayNumeri);
+
+        while (arrayUtente.length < 34 && semaforo == true) {
+            var utente = parseInt(prompt("Inserisci un numero"));
+        
+            for (i = 0; i < arrayNumeri.length; i++) {
+                if (arrayNumeri[i] == utente) {
+                    semaforo = false;
+                    alert("hai perso");
+                } 
+            }
+            if (semaforo == true) {
+                if(utente >= 1 && utente <= 50) {
+                    message = true;
+                    // console.log(utente);
+                } else {
+                    message = false;
+                }
+                if ((arrayUtente.includes(utente) == false) && (message == true)) {
+                    arrayUtente.push(utente);
+                } else if (message == false) {
+                    alert("Intervallo errato");
+                } else {
+                    alert("Non puoi inserire lo stesso numero più volte");
+                }
+            }
+        }
+        
+        console.log(arrayUtente);
+        
+        // definisco il punteggio finale 
+        
+        for (i = 0; i < arrayUtente.length; i++) {
+            var vincitore = (i + 1);
+        }
+        
+        document.getElementById('punteggio').innerHTML += "Il tuo punteggiò è di " + vincitore;
+        
+        
+        if (vincitore == 34) {
+            document.getElementById('campione').innerHTML = "Complimenti hai vinto!"
+        }
+
+        break;
 }
 
-for (i = 0; i < arrayUtente.length; i++) {
-    var vincitore = (i + 1);
-}
-
-document.getElementById('punteggio').innerHTML += "Il tuo punteggiò è di " + vincitore;
 
 
 
